43bc76a2efc7a8ab83fcabb3fb513365f7a69514
andrei.pugatsov <andrei.pugatsov@gmail.com>
2024-09-15 15:21:56 +0300

    remaking GuessTheCocktailGameApplication for web

--- src/main/java/com/example/cocktail_game/GuessTheCocktailGameApplication.java
+++ src/main/java/com/example/cocktail_game/GuessTheCocktailGameApplication.java
@@ -1,23 +1,12 @@
 package com.example.cocktail_game;
 
-import com.example.cocktail_game.domain.CocktailGame;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.web.client.RestTemplate;
 
-//@ComponentScan(basePackages = "com.example.cocktail_game")
 @SpringBootApplication
 public class GuessTheCocktailGameApplication {
-
-    private final CocktailGame cocktailGame;
-
-    public GuessTheCocktailGameApplication(@Lazy CocktailGame cocktailGame) {
-        this.cocktailGame = cocktailGame;
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(GuessTheCocktailGameApplication.class, args);
@@ -26,19 +15,5 @@ public class GuessTheCocktailGameApplication {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
-    }
-
-    @Bean
-    public ApplicationRunner applicationRunner() {
-        return args -> {
-            try {
-                cocktailGame.startGame();
-            } catch (Exception e) {
-                System.err.println("An error occurred while running the game: " + e.getMessage());
-                e.printStackTrace();
-            } finally {
-                System.exit(0);
-            }
-        };
     }
 }
